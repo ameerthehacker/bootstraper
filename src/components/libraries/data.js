96c95676184212daf81ffa776645324589c973d5
Ameer Jhan <ameerjhanprof@gmail.com>
2020-01-08 14:06:43 +0530

    fix: use a placeholder image for time being

--- src/components/libraries/data.js
+++ src/components/libraries/data.js
@@ -1,24 +1,22 @@
-import tsImage from './images/ts.png';
-import flowImage from './images/flow.jpg';
-import babelImage from './images/babel.png';
+import placeholderImage from './images/icon-placeholder.png';
 
 export const LIBRARIES = {
   babel: {
     title: 'Babel',
     description: `Babel converts ECMAScript 2015+ code into a backwards compatible version of JavaScript`,
     learnMoreLink: 'https://www.typescriptlang.org/docs/home.html',
-    image: babelImage
+    image: placeholderImage
   },
   typescript: {
     title: 'TypeScript',
     description: `TypeScript is a typed superset of JavaScript by Microsoft`,
     learnMoreLink: 'https://www.typescriptlang.org/docs/home.html',
-    image: tsImage
+    image: placeholderImage
   },
   flow: {
     title: 'Flow',
     description: `Flow is a typed superset of JavaScript by Facebook`,
     learnMoreLink: 'https://flow.org/en/docs/',
-    image: flowImage
+    image: placeholderImage
   }
 };
